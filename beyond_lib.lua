de7d9aba6d09c54dfdc22db74125548bac80c3ad
NizonRox <nizon.xd@gmail.com>
2021-09-07 19:37:51 +0200

    1.0.4 - GoAlS [NT] [NF]

--- beyond_lib.lua
+++ beyond_lib.lua
@@ -3,13 +3,21 @@ cur_xpos_g = 0
 cur_ypos_g = 0
 fr_width_g = 75
 fr_height_g = 20
-b_frame_array = { "IT", "IA", "IPT", "CT", "CA", "CPT", "AT", "AA", "APT", "PT", "MA", "PA", "MP", "PPT" }
+beyond_f_array = { "IT", "IA", "IPT", "CT", "CA", "CPT", "AT", "AA", "APT", "PT", "MA", "PA", "MP", "PPT" }
+beyond_c_array = { "000011", "000022", "000033", "000044", "000055", "000066", "000077", "000088", "000099", "0000AA", "0000BB", "0000BB", "0000CC", "0000DD", "0000EE" }
+beyond_debug = 0
 
-function btp_frame_debug(msg)
-    DEFAULT_CHAT_FRAME:AddMessage(msg)
-    UIErrorsFrame:AddMessage(msg, 1.0, 1.0, 0, 1, 10)
+-- Debug message for trouble shooting; With On/Off option via variable
+function beyond_lib_debug(msg)
+    if beyond_debug=1 then
+        DEFAULT_CHAT_FRAME:AddMessage(msg)
+        UIErrorsFrame:AddMessage(msg, 1.0, 1.0, 0, 1, 10)
+    Else
+        return
+    end
 end
 
+-- Calculate where the next frame should be (Used in btp_frame_onload)
 function btp_next_cord()
     local curx
     local cury
@@ -17,27 +25,20 @@ function btp_next_cord()
     cury = cur_ypos_g
 
     cur_xpos_g = cur_xpos_g + fr_width_g
-    -- btp_frame_debug("xpos: " .. curx .. " ypos: " .. cury);
+    beyond_lib_debug("xpos: " .. curx .. " ypos: " .. cury)
     return curx, cury
 end
 
-function btp_frame_init()
-    btp_frame_set_color_hex("IT", "000011")
-    btp_frame_set_color_hex("IA", "000022")
-    btp_frame_set_color_hex("IPT", "000033")
-    btp_frame_set_color_hex("CT", "000044")
-    btp_frame_set_color_hex("CA", "000055")
-    btp_frame_set_color_hex("CPT", "000066")
-    btp_frame_set_color_hex("AT", "000077")
-    btp_frame_set_color_hex("AA", "000088")
-    btp_frame_set_color_hex("APT", "000099")
-    btp_frame_set_color_hex("PT", "0000AA")
-    btp_frame_set_color_hex("MA", "0000BB")
-    btp_frame_set_color_hex("PA", "0000CC")
-    btp_frame_set_color_hex("MP", "0000DD")
-    btp_frame_set_color_hex("PPT", "0000EE")
+-- Loops over each frame and initializes them by changing their hex
+function beyond_lib_init()
+    for i, v in pairs(beyond_f_array) do
+        curx, cury = btp_next_cord()
+        beyond_lib_debug("Change: " .. beyond_f_array[v] .. " Hex: " .. beyond_c_array[v])
+        btp_frame_set_color_hex(beyond_f_array[v], beyond_c_array[v])
+    end
 end
 
+-- Define slash commands/ Creation of all frames
 function btp_frame_onload(self)
     local curx
     local cury
@@ -46,36 +47,33 @@ function btp_frame_onload(self)
     curw = fr_width_g
     curh = fr_height_g
 
-    -- btp_frame_debug("btp_frame: OnLoad");
+    beyond_lib_debug("btp_frame: OnLoad")
     SLASH_BTP_FRAME1 = "/btp_frame"
     SLASH_BTP_INIT1 = "/btp_finit"
     SLASH_BTP_DBG1 = "/btp_dbg"
 
     SlashCmdList["BTP_FRAME"] = function(cmdstr)
-        -- btp_frame_debug("cmdstr: " .. cmdstr);
+        -- btp_frame_debug("cmdstr: " .. cmdstr)
         btp_frame_runcmd(cmdstr)
     end
 
-    SlashCmdList["BTP_INIT"] = btp_frame_init
+    SlashCmdList["BTP_INIT"] = beyond_lib_init
 
     SlashCmdList["BTP_DBG"] = function(cmdstr)
         -- btp_frame_debug(cmdstr);
     end
-
-    for i, v in pairs(b_frame_array) do
+    --Loop creation of bars
+    for i, v in pairs(beyond_f_array) do
     curx, cury = btp_next_cord()
     btp_frame_create(v[i], curx, cury, curw, curh)
     end
 end
 
-function btp_frame_print_usage(cmdstr)
-    --btp_frame_debug("/pk [0-9] [arg1 ... argn]");
-end
-
+-- Hide/Show function
 function btp_frame_toggle(fname)
     local full_name = "btp_frame_" .. fname
     local frame = getglobal(full_name)
-    --btp_frame_debug(full_name);
+    --btp_frame_debug(full_name)
     if (frame) then
         if (frame:IsVisible()) then
             frame:Hide()
